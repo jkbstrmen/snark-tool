1314ba6a0e1c6882dfa447b444714db098bd4422
jstrmen <kubbost@gmail.com>
2020-04-27 20:38:36 +0200

    sat colouriser cleanup

--- src/test/colour_test.rs
+++ src/test/colour_test.rs
@@ -9,6 +9,7 @@ pub mod colour_tests {
     use crate::service::io::reader_g6::G6Reader;
     use crate::test::test_data::test_data;
     use std::fs::OpenOptions;
+    use crate::service::colour::sat::SATColourizer;
 
     #[test]
     fn should_be_snark_bfs() {
@@ -43,58 +44,38 @@ pub mod colour_tests {
     #[test]
     fn should_be_snark_sat() {
         let graph = test_data::get_petersen_graph();
-        let result = sat::is_colorable(&graph);
+        let result = SATColourizer::is_colorable(&graph);
         assert_eq!(result, false);
 
         let graph = G6Reader::<SimpleGraph>::read_graph(test_data::SNARK_IN_G6_20);
-        let result = sat::is_colorable(&graph.unwrap());
+        let result = SATColourizer::is_colorable(&graph.unwrap());
         assert_eq!(result, false);
 
         let graph = G6Reader::<SimpleGraph>::read_graph(test_data::SNARK_IN_G6_36);
-        let result = sat::is_colorable(&graph.unwrap());
+        let result = SATColourizer::is_colorable(&graph.unwrap());
         assert_eq!(result, false);
 
         let graph = G6Reader::<SimpleGraph>::read_graph(test_data::SNARK_IN_G6_40);
-        let result = sat::is_colorable(&graph.unwrap());
+        let result = SATColourizer::is_colorable(&graph.unwrap());
         assert_eq!(result, false);
     }
 
     #[test]
     fn should_be_colourable_sat() {
         let graph = G6Reader::<SimpleGraph>::read_graph(test_data::NO_SNARK_IN_G6_18);
-        let result = sat::is_colorable(&graph.unwrap());
+        let result = SATColourizer::is_colorable(&graph.unwrap());
         assert_eq!(result, true);
 
         let graph = G6Reader::<SimpleGraph>::read_graph(test_data::NO_SNARK_IN_G6_112);
-        let result = sat::is_colorable(&graph.unwrap());
+        let result = SATColourizer::is_colorable(&graph.unwrap());
         assert_eq!(result, true);
     }
 
     #[test]
-    fn should_color_sat() {
-        // let graph = G6Reader::<SimpleGraph>::read_graph(test_data::SNARK_IN_G6_10_PETERSEN);
-        // let graph = G6Reader::<SimpleGraph>::read_graph(test_data::SNARK_IN_G6_20);
-        // let graph = G6Reader::<SimpleGraph>::read_graph(test_data::SNARK_IN_G6_36);
-        // let graph = G6Reader::<SimpleGraph>::read_graph(test_data::SNARK_IN_G6_40);
+    fn run_cvd(){
 
-        // let graph = G6Reader::<SimpleGraph>::read_graph(test_data::NO_SNARK_IN_G6_18);
-        // let graph = G6Reader::<SimpleGraph>::read_graph(test_data::NO_SNARK_IN_G6_112);
 
-        let file = OpenOptions::new()
-            .read(true)
-            .open("resources/graphs/PSC6XJ5.118")
-            .unwrap();
-        let mut reader = BaReader::<SimpleGraph>::new(&file);
-        let graph = reader.next().unwrap();
 
-        let solution = sat::is_colorable(&graph.unwrap());
-        println!("solution: {}", solution);
 
-        // sat::resolve();
     }
 }
-
-// #[cfg(test)]
-// pub mod colour_tests_sat {
-//
-// }

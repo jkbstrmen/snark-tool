f233123aba439a4d38012a791153548a40de78ca
jstrmen <kubbost@gmail.com>
2020-11-04 20:58:05 +0100

    WIP added - simple and naive impelementation of DFS

--- src/service/colour/dfs_naive.rs
+++ src/service/colour/dfs_naive.rs
@@ -1,11 +1,11 @@
 use crate::graph::edge::Edge;
 use crate::graph::graph::Graph;
-use crate::graph::vertex::Vertex;
 use crate::service::colour::colouriser::Colourizer;
-use crate::service::colour::cvd::DFS_CALLS;
+use std::collections;
+use std::collections::VecDeque;
+use crate::graph::vertex::Vertex;
 
-// Colorizer for (sub)cubic graphs only
-pub struct DFSColourizer {}
+pub struct DFSColourizerNaive {}
 
 struct BFSColourizerGraph {
     // pair - (neighbor, color)
@@ -14,14 +14,12 @@ struct BFSColourizerGraph {
     one_edge_vert: Vec<usize>,
     non_colored_edges: Vec<usize>,
     non_colored_edges_of_graph: usize,
+
+    queue: collections::VecDeque<usize>
 }
 
-impl Colourizer for DFSColourizer {
+impl Colourizer for DFSColourizerNaive {
     fn is_colorable<G: Graph>(graph: &G) -> bool {
-        // unsafe {
-        //     DFS_CALLS += 1;
-        // }
-
         let mut vertices = Vec::with_capacity(graph.size());
         // create local graph
         for vertex in graph.vertices() {
@@ -43,6 +41,7 @@ impl Colourizer for DFSColourizer {
             one_edge_vert: vec![],
             non_colored_edges: vec![],
             non_colored_edges_of_graph: 0,
+            queue: collections::VecDeque::new()
         };
 
         // precolor
@@ -69,39 +68,186 @@ impl Colourizer for DFSColourizer {
                 break;
             }
         }
-        let mut non_colored_edges_of_graph = 0;
-        let mut non_colored_edges_of_vertex_count = vec![0; graph.size()];
-        let mut one_edge_vert = vec![];
-        let mut i = 0;
-        for vertex in &color_graph.vertices {
-            for neighbor in vertex.iter() {
-                if neighbor.1 == 1 {
-                    non_colored_edges_of_vertex_count[i] += 1;
-                    non_colored_edges_of_graph += 1;
-                }
-            }
-            if non_colored_edges_of_vertex_count[i] == 1 {
-                if vertex[2].1 != 0 {
-                    one_edge_vert.push(i);
-                }
-            }
-            i += 1;
-        }
-        non_colored_edges_of_graph = non_colored_edges_of_graph / 2;
 
-        color_graph.one_edge_vert = one_edge_vert;
-        color_graph.non_colored_edges = non_colored_edges_of_vertex_count;
-        color_graph.non_colored_edges_of_graph = non_colored_edges_of_graph;
-        // println!("first vertex: {}", first_vertex);
-        color_graph.color(first_vertex)
+        color_graph.queue.push_back(first_vertex);
+
+        unsafe {
+            color_graph.color_recursive()
+        }
     }
 
+    // fn is_colorable<G: Graph>(graph: &G) -> bool {
+    //     // create local graph
+    //     let graph_matrix = vec![0; graph.size() * graph.size()];
+    //     let mut colour_graph = BFSColourizerGraph2 {
+    //         graph: graph_matrix,
+    //         neighbors: vec![[0, 0, 0]; graph.size()],
+    //         one_edge_vert: vec![],
+    //         non_colored_edges_count: vec![0; graph.size()],
+    //         non_colored_edges_of_graph: 0,
+    //         graph_size: graph.size(),
+    //     };
+    //     for edge in graph.edges() {
+    //         colour_graph.set_edge_color(edge.from(), edge.to(), 1);
+    //     }
+    //
+    //     // precolor first vertex
+    //     let mut first_vertex = 0;
+    //     let colours = [3, 4, 5];
+    //     let mut vertex_coloured = false;
+    //     let mut vertex = 0;
+    //     let mut colour_position = 0;
+    //     while !vertex_coloured {
+    //         for neighbor in 0..graph.size() {
+    //             if colour_graph.get_edge_color(vertex, neighbor) == 1 {
+    //                 colour_graph.set_edge_color(vertex, neighbor, colours[colour_position]);
+    //                 colour_position += 1;
+    //                 first_vertex = vertex;
+    //                 vertex_coloured = true;
+    //             }
+    //         }
+    //         vertex += 1;
+    //     }
+    //     colour_graph.non_colored_edges_of_graph = 0;
+    //
+    //     // get count of non colored edges for whole graph and for vertices, init neighbors
+    //     for vertex in 0..graph.size() {
+    //         let mut neighbor_position = 0;
+    //         colour_graph.non_colored_edges_count[vertex] = 0;
+    //         for neighbor in 0..graph.size() {
+    //             if colour_graph.get_edge_color(vertex, neighbor) > 0 {
+    //                 colour_graph.neighbors[vertex][neighbor_position] = neighbor;
+    //                 neighbor_position += 1;
+    //                 if colour_graph.get_edge_color(vertex, neighbor) == 1 {
+    //                     colour_graph.non_colored_edges_of_graph += 1;
+    //                     colour_graph.non_colored_edges_count[vertex] += 1;
+    //                 }
+    //             }
+    //         }
+    //         if neighbor_position < 3 {
+    //             colour_graph.neighbors[vertex][neighbor_position] = vertex;
+    //         }
+    //         neighbor_position += 1;
+    //         if neighbor_position < 3 {
+    //             colour_graph.neighbors[vertex][neighbor_position] = vertex;
+    //         }
+    //     }
+    //
+    //     colour_graph.non_colored_edges_of_graph = colour_graph.non_colored_edges_of_graph / 2;
+    //     for vertex in 0..graph.size() {
+    //         if colour_graph.non_colored_edges_count[vertex] == 1 {
+    //             colour_graph.one_edge_vert.push(vertex);
+    //         }
+    //     }
+    //     colour_graph.color(first_vertex)
+    // }
+
     fn new() -> Self {
-        DFSColourizer {}
+        Self {}
     }
 }
 
+impl DFSColourizerNaive {}
+
 impl BFSColourizerGraph {
+
+
+    unsafe fn color_recursive(&mut self) -> bool {
+        if self.queue.is_empty() {
+            return true;
+        }
+        let vertex = self.queue.pop_back().unwrap();
+        let available_colors = self.available_colors_of_vertex(vertex);
+        if available_colors.len() == 0 {
+            for index in 0..self.vertices.len() {
+                let av = self.available_colors_of_vertex(index).len();
+                if av > 0 && av < 3 {
+                    self.queue.push_back(index);
+                    if self.color_recursive() {
+                        return true;
+                    }
+                    self.queue.pop_back();
+                }
+            }
+            return false;
+        }
+        let self_ref = self as *const BFSColourizerGraph;
+        if available_colors.len() == 1 {
+            for neighbor in (*self_ref).vertices[vertex].iter() {
+                if neighbor.1 == 1 {
+                    self.set_edge_color(vertex, neighbor.0, available_colors[0]);
+                    let without_conflict = self.is_vertex_without_conflict(&self.vertices[neighbor.0]);
+                    if without_conflict {
+                        self.queue.push_back(neighbor.0);
+                        if self.color_recursive() {
+                            return true;
+                        }
+                        self.queue.pop_back();
+                    }
+                    self.set_edge_color(vertex, neighbor.0, 1);
+                }
+            }
+            // panic!("we could not find non coloured vertex, even if we have available colour");
+        }
+        if available_colors.len() == 2 {
+            let mut available_colours_variations = vec![];
+            available_colours_variations.push(vec![available_colors[0], available_colors[1]]);
+            available_colours_variations.push(vec![available_colors[1], available_colors[0]]);
+            for available_colours_variation in available_colours_variations {
+
+                let mut first_neighbor = 0;
+                let mut second_neighbor = 0;
+                for neighbor in (*self_ref).vertices[vertex].iter() {
+                    if neighbor.1 == 1 {
+                        first_neighbor = second_neighbor;
+                        second_neighbor = neighbor.0;
+                    }
+                }
+
+                self.set_edge_color(vertex, first_neighbor, available_colours_variation[0]);
+                let without_conflict = self.is_vertex_without_conflict(&self.vertices[first_neighbor]);
+                if !without_conflict {
+                    self.set_edge_color(vertex, first_neighbor, 1);
+                    continue;
+                }
+                self.set_edge_color(vertex, second_neighbor, available_colours_variation[1]);
+                let without_conflict = self.is_vertex_without_conflict(&self.vertices[second_neighbor]);
+                if !without_conflict {
+                    self.set_edge_color(vertex, first_neighbor, 1);
+                    continue;
+                }
+                self.queue.push_back(first_neighbor);
+                // self.queue.push_back(second_neighbor);
+
+                if self.color_recursive() {
+                    return true;
+                }
+
+                // pop_back??
+                self.queue.pop_back();
+                // self.queue.pop_back();
+
+                self.set_edge_color(vertex, first_neighbor, 1);
+                self.set_edge_color(vertex, second_neighbor, 1);
+            }
+        }
+
+        self.queue.push_back(vertex);
+        false
+    }
+
+    fn available_colors_of_vertex(&self, vertex: usize) -> Vec<usize> {
+        let mut colors = vec![3, 4, 5];
+        for neighbor in self.vertices[vertex].iter() {
+            colors.retain(|&item| item != neighbor.1)
+        }
+        colors
+    }
+
+
+
+
+    // ============================================================
     fn color(&mut self, vertex: usize) -> bool {
         let color_vars = [(4, 5), (3, 5), (3, 4)];
 
@@ -335,6 +481,7 @@ impl BFSColourizerGraph {
         self.set_color_and_check_validity(from, to, color) && self.color(next_vertex)
     }
 }
+
 
 fn is_without_conflict(color1: usize, color2: usize, color3: usize) -> bool {
     // ttt
